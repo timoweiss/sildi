c9886bec57b5db4c87557f28a4cb2671356b2d11
Timo Weiß <info@timo-weiss.com>
2014-07-16 23:26:08 +0200

    remove unused code

--- public/recorder.js
+++ public/recorder.js
@@ -1,7 +1,7 @@
-(function (window) {
+(function(window) {
     var socket = io('http://localhost:3700');
 
-    socket.on('readyForStream', function () {
+    socket.on('readyForStream', function() {
         console.log('readyForStream');
 
 
@@ -12,11 +12,10 @@
         if (navigator.getUserMedia) {
             navigator.getUserMedia({
                 audio: true
-            }, success, function (e) {
+            }, success, function(e) {
                 alert('Error capturing audio.');
             });
-        }
-        else alert('getUserMedia not supported in this browser.');
+        } else alert('getUserMedia not supported in this browser.');
 
         var recording = true;
 
@@ -30,7 +29,7 @@
             var bufferSize = 2048;
             recorder = context.createJavaScriptNode(bufferSize, 1, 1);
 
-            recorder.onaudioprocess = function (e) {
+            recorder.onaudioprocess = function(e) {
                 if (!recording) return;
                 var left = e.inputBuffer.getChannelData(0);
                 socket.emit('streaming', {
@@ -44,7 +43,7 @@
         }
 
 
-        socket.on('back', function (data) {
+        socket.on('back', function(data) {
             //play(data.chunk);
             // play(data);
             console.log(new DataView(data.chunk));
@@ -56,68 +55,6 @@
             //     //console.error(arguments);
             // });
         });
-
-
-        function processConcatenatedFile(data) {
-
-            var bb = new DataView(data);
-            var offset = 0;
-
-            while (offset < bb.byteLength) {
-
-                var length = bb.getUint32(offset, true);
-                offset += 4;
-                var sound = extractBuffer(data, offset, length);
-                offset += length;
-
-                createSoundWithBuffer(sound.buffer);
-
-            }
-
-        }
-
-        function extractBuffer(src, offset, length) {
-
-            var dstU8 = new Uint8Array(length);
-            var srcU8 = new Uint8Array(src, offset, length);
-            dstU8.set(srcU8);
-            return dstU8;
-
-        }
-
-        function createSoundWithBuffer(buffer) {
-
-            /*
-        This audio context is unprefixed!
-    */
-            var context = new AudioContext();
-
-            var audioSource = context.createBufferSource();
-            audioSource.connect(context.destination);
-
-            context.decodeAudioData(buffer, function (res) {
-
-                audioSource.buffer = res;
-
-                /*
-           Do something with the sound, for instance, play it.
-           Watch out: all the sounds will sound at the same time!
-        */
-                audioSource.noteOn(0);
-
-            });
-
-        }
-
-
-        function play(incomingData) {
-            source = context.createBufferSource(); // Create Sound Source
-            //buffer = context.createBuffer(incomingData, true); // Create Mono Source Buffer from Raw Binary
-            source.buffer = incomingData; // Add Buffered Data to Object
-            source.connect(context.destination); // Connect Sound Source to Output
-            //        source.start(0);
-            source.noteOn(context.currentTime); // Play the Source when Triggered
-        }
 
     });
 
